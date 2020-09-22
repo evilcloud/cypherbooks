ecd609943f6c24e1144d6dc37f48498c4e1fe009
evilcloud <mrgkail@gmail.com>
2020-09-22 10:34:16 +0800

    wtf

--- utils/chapters.nim
+++ utils/chapters.nim
@@ -2,8 +2,9 @@ import strutils
 import strformat
 import re
 
-let fileContent = readFile("../cypher/delillo_white_noise.cypher")
 
+
+let fileContent = readFile("../cypher/delillo_white_noise.cypher")
 let fileLines = splitLines(fileContent)
 echo fmt"...loaded {fileLines.len} strings"
 
