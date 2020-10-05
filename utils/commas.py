7526d8d1025ef76597406fc726a1b81a910ee04e
evilcloud <mrgkail@gmail.com>
2020-10-06 02:20:39 +0800

    improving

--- utils/commas.py
+++ utils/commas.py
@@ -1,7 +1,25 @@
+#! /usr/bin/python3
+
 import os
 import sys
+import re
 
-# TODO: remove last comma in the declarations. Almost done, but something went wrong
+# TODO: Make the real last comma declaration, not the fake ()
+# TODO: Check the presense of edge directions
+# TODO:
+
+
+class color:
+    PURPLE = "\033[95m"
+    CYAN = "\033[96m"
+    DARKCYAN = "\033[36m"
+    BLUE = "\033[94m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    END = "\033[0m"
 
 
 def get_file_lines(filename: str) -> list:
@@ -14,7 +32,7 @@ def get_file_lines(filename: str) -> list:
         list: line-by-line content
     """
     if not os.path.isfile(filename):
-        print(f"File {filename} not found")
+        print(color.RED + f"File {filename} not found" + color.END)
         quit()
     with open(filename, "r") as f:
         return f.readlines()
@@ -52,46 +70,54 @@ def argument(arg: list) -> str:
     if len(cypher) == 1:
         return cypher[0]
 
-    print("No cypher file given of found")
+    print(color.RED + "No cypher file given of found" + color.END)
     quit()
 
 
-def add_commas(lines: list) -> list:
-    """
-    Checks for commas at the end of each line, that starts with a ( and ends with a )
-    Also elliminates the comma at the end of last declaration, if such is present
-    """
-    new_lines = []
+def add_commas(line: str) -> str:
+    if line.strip().startswith("("):
+        if line.strip().endswith(")"):
+            return line.strip("\n") + ",\n"
+        if ") #" in line:
+            part1, part2 = line.rsplit(")")
+            return part1 + ")," + part2
+    return None
+
+
+def exception(line) -> bool:
+    return line.strip() == "()"
+
+
+def check_lines(lines: list) -> list:
     added = 0
-    for line in lines:
-        if line.strip().startswith("(") and line.strip().endswith(")"):
-            new_lines.append(line.strip("\n") + ",\n")
+    new_lines = []
+    for i, line in enumerate(lines):
+        if exception(line) or i == len(lines):
+            continue
+        with_commas = add_commas(line)
+        if line := with_commas:
+            lines[i] = line
+            print(f"{color.BLUE + str(i+1) + color.END}", line.strip("\n"))
             added += 1
-        else:
-            new_lines.append(line)
-    print(f"{added} missing commas added")
-    return new_lines
 
-
-def comparative_print(lines, new_lines: list):
-    for a, b in zip(lines, new_lines):
-        print("l", a)
-        print("n", b)
-        print()
+    print(f"\nCommas added: {color.BOLD + str(added) + color.END}")
+    return lines if added else None
 
 
 def main():
     filename = argument(sys.argv)
-    print(f"working with {filename}")
+    # filename = (
+    #     "/Users/kg/Documents/code/cypherbooks/utils/Philosophical_posthumanism.cypher"
+    # )
+    print(f"Checking {color.BOLD + filename + color.END}")
     lines = get_file_lines(filename)
-    new_lines = add_commas(lines)
-    if new_lines == lines:
-        print("No changes neccesary")
-    else:
+    new_lines = check_lines(lines)
+    if new_lines:
         input("ENTER to implement changes to the file")
         save_file(filename, new_lines)
-        # comparative_print(lines, new_lines)
-    print("All done\n...buy")
+    else:
+        print(color.BLUE + "No changes needed" + color.END)
+    print("All done\n...bye")
 
 
 if __name__ == "__main__":
